7fdc86fd38eab3a1d215a1c43d7b4b7561a22907
marcelodolacio <marcelodolacio@gmail.com>
2022-05-07 23:37:23 -0300

    chamada de mapas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+declare const google:any
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'prove';
+export class AppComponent implements OnInit, AfterViewInit{
+
+  dados:any
+  options: any;
+  element:any;
+
+  @ViewChild('mapElement') mapElement:any
+  constructor(){
+    this.dados =[{nome:'Marcelo', cidade:'Brasilia'},{nome:'Renata', cidade:'Planaltina'}, {nome:'Nilson', cidade:'São Paulo'}];
+
+  }
+  ngOnInit() {
+    
+    
+  }
+  ngAfterViewInit() {
+        this.element = new google.maps.Map(this.mapElement.nativeElement, {
+        center: {lat: -15.7801, lng:-47.9292},
+        zoom: 14
+       
+      })
+    
+  }
+  buscaMapa(elemento:any){
+    console.log('euuuu', elemento)
+  
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {GMapModule} from 'primeng/gmap';
+// import { MapsComponent } from './maps/maps.component';
+import { GooglePlaceModule } from 'ngx-google-places-autocomplete'; 
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // MapsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    TableModule,
+    GMapModule,
+    GooglePlaceModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBSbUSjaFFRK336-uv7CIDd9bDHCTZM-Ek',
+      libraries: ['places']
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
